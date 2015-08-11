5f48465cc3e12056b406997832ab9717489de72a
Chuntao Hong <chuntao.hong@gmail.com>
2015-08-11 15:13:28 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+SetWallPaper: c# console program that sets Windows wallpaper.
+
+Requires: .NET framework 4.0
+
+Usage: SetWallPaper.exe image.jpg [center/tile/stretch]
---
+++ SetWallpaper/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Drawing;
+using System.IO;
+using Microsoft.Win32;
+
+namespace SetWallpaper
+{
+    public sealed class Wallpaper
+    {
+        Wallpaper() { }
+
+        const int SPI_SETDESKWALLPAPER = 20;
+        const int SPIF_UPDATEINIFILE = 0x01;
+        const int SPIF_SENDWININICHANGE = 0x02;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+
+        public enum Style : int
+        {
+            Tiled,
+            Centered,
+            Stretched
+        }
+
+        public static void Set(String path, String styleString)
+        {
+            Style style = Style.Centered;
+            if (styleString == "tile")
+                style = Style.Tiled;
+            else if (styleString == "stretched")
+                style = Style.Stretched;
+            System.IO.Stream s = new System.Net.WebClient().OpenRead(path);
+
+            Image img = Image.FromStream(s);
+            string tempPath = Path.Combine(Path.GetTempPath(), "wallpaper.bmp");
+            img.Save(tempPath, System.Drawing.Imaging.ImageFormat.Bmp);
+
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Control Panel\Desktop", true);
+            if (style == Style.Stretched)
+            {
+                key.SetValue(@"WallpaperStyle", 2.ToString());
+                key.SetValue(@"TileWallpaper", 0.ToString());
+            }
+
+            if (style == Style.Centered)
+            {
+                key.SetValue(@"WallpaperStyle", 1.ToString());
+                key.SetValue(@"TileWallpaper", 0.ToString());
+            }
+
+            if (style == Style.Tiled)
+            {
+                key.SetValue(@"WallpaperStyle", 1.ToString());
+                key.SetValue(@"TileWallpaper", 1.ToString());
+            }
+
+            SystemParametersInfo(SPI_SETDESKWALLPAPER,
+                0,
+                tempPath,
+                SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                System.Console.WriteLine("usage: SetWallPaper filePath [center/tile/strech]");
+            }
+            string file = args[0];
+            string style = "center";
+            if (args.Length >= 2)
+                style = args[1];
+            Wallpaper.Set(file, style);
+        }
+    }
+}
