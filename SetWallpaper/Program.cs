a67a4f4d276727bc3428c9798bcd3e9e633dca2a
Chuntao Hong <chuntao.hong@gmail.com>
2020-01-03 17:03:47 +0800

    add more modes

--- SetWallpaper/Program.cs
+++ SetWallpaper/Program.cs
@@ -23,18 +23,29 @@ namespace SetWallpaper
 
         public enum Style : int
         {
-            Tiled,
-            Centered,
-            Stretched
+            Fill,
+            Fit,
+            Span,
+            Stretch,
+            Tile,
+            Center            
         }
 
         public static void Set(String path, String styleString)
         {
-            Style style = Style.Centered;
-            if (styleString == "tile")
-                style = Style.Tiled;
-            else if (styleString == "stretched")
-                style = Style.Stretched;
+            Style style = Style.Center;
+            if (styleString == "fill")
+                style = Style.Fill;
+            else if (styleString == "fit")
+                style = Style.Fit;
+            else if (styleString == "span")
+                style = Style.Span;
+            else if (styleString == "stretch")
+                style = Style.Stretch;
+            else if (styleString == "tile")
+                style = Style.Tile;
+            else if (styleString == "center")
+                style = Style.Center;
             System.IO.Stream s = new System.Net.WebClient().OpenRead(path);
 
             Image img = Image.FromStream(s);
@@ -42,22 +53,35 @@ namespace SetWallpaper
             img.Save(tempPath, System.Drawing.Imaging.ImageFormat.Bmp);
 
             RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Control Panel\Desktop", true);
-            if (style == Style.Stretched)
+            if (style == Style.Fill)
+            {
+                key.SetValue(@"WallpaperStyle", 10.ToString());
+                key.SetValue(@"TileWallpaper", 0.ToString());
+            }
+            if (style == Style.Fit)
+            {
+                key.SetValue(@"WallpaperStyle", 6.ToString());
+                key.SetValue(@"TileWallpaper", 0.ToString());
+            }
+            if (style == Style.Span) // Windows 8 or newer only!
+            {
+                key.SetValue(@"WallpaperStyle", 22.ToString());
+                key.SetValue(@"TileWallpaper", 0.ToString());
+            }
+            if (style == Style.Stretch)
             {
                 key.SetValue(@"WallpaperStyle", 2.ToString());
                 key.SetValue(@"TileWallpaper", 0.ToString());
             }
-
-            if (style == Style.Centered)
+            if (style == Style.Tile)
             {
-                key.SetValue(@"WallpaperStyle", 1.ToString());
-                key.SetValue(@"TileWallpaper", 0.ToString());
-            }
-
-            if (style == Style.Tiled)
-            {
-                key.SetValue(@"WallpaperStyle", 1.ToString());
+                key.SetValue(@"WallpaperStyle", 0.ToString());
                 key.SetValue(@"TileWallpaper", 1.ToString());
+            }
+            if (style == Style.Center)
+            {
+                key.SetValue(@"WallpaperStyle", 0.ToString());
+                key.SetValue(@"TileWallpaper", 0.ToString());
             }
 
             SystemParametersInfo(SPI_SETDESKWALLPAPER,
@@ -74,7 +98,8 @@ namespace SetWallpaper
         {
             if (args.Length < 1)
             {
-                System.Console.WriteLine("usage: SetWallPaper filePath [center/tile/strech]");
+                System.Console.WriteLine("usage: SetWallPaper filePath [fill/fit/span/stretch/tile/center]");
+                return;
             }
             string file = args[0];
             string style = "center";
